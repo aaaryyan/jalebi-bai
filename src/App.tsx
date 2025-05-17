23e9dfa25c8a18616838f2ffc3b45dd7d8d929b1
aaaryyan <aaryankrmishra2006@gmail.com>
2025-05-17 15:07:06 +1000

    debugging

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@
 
 import React, { useState, useMemo } from 'react'
 import OrbitSettings from './components/OrbitSettings'
-import SimulationVisualiser from './components/SimulationVisualiser'
+import SimulationVisualizer from './components/SimulationVisualiser'
 import StatsPanel from './components/StatsPanel'
 import { OrbitParams } from './types'
 import { calculateOrbit } from './logic/orbitMath'
 import { calculateHandshakes } from './logic/handshakeSim'
 import './App.css'
 
-// Temporary fixed relay satellite configuration
+//  Temporary fixed relay satellite config
 const defaultRelayParams: OrbitParams = {
   orbitType: 'Non-polar',
   altitude: 780,
   inclination: 98.6,
-  localSolarTime: '12:00',
+  localSolarTime: '12:00', // unused for non-polar but required by type
 }
 
 const App: React.FC = () => {
@@ -25,8 +25,8 @@ const App: React.FC = () => {
     localSolarTime: '12:00',
   })
 
-  const beaconPath = useMemo( () => calculateOrbit(params), [params])
-  const relayPath = useMemo( () => calculateOrbit(defaultRelayParams), [])
+  const beaconPath = useMemo(() => calculateOrbit(params), [params])
+  const relayPath = useMemo(() => calculateOrbit(defaultRelayParams), [])
   const handshakeResult = useMemo(
     () => calculateHandshakes(beaconPath, relayPath),
     [beaconPath, relayPath]
@@ -34,12 +34,14 @@ const App: React.FC = () => {
 
   return (
     <div className="app">
-      <h1> Space Satellite Handshakes Visualiser</h1>
+      <h1>🛰️ Space Satellite Handshakes Visualizer</h1>
+
       <OrbitSettings params={params} setParams={setParams} />
-      <SimulationVisualiser beaconPath={beaconPath} relayPath={relayPath} />
+      <SimulationVisualizer beaconPath={beaconPath} relayPath={relayPath} />
       <StatsPanel result={handshakeResult} />
     </div>
   )
 }
 
 export default App
+
